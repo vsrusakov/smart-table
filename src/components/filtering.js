0c7e6fc98f1fe4afae4fca3632f44c42fd877491
vladimir-rusakov <virt.p.q@yandex.ru>
2026-02-08 02:37:42 +0300

    добавил фильтрацию

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,33 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)
+        .forEach((elementName) => {
+            elements[elementName].append(
+                ...Object.values(indexes[elementName])
+                    .map(name => {
+                        // @todo: создать и вернуть тег опции
+                        const option = document.createElement('option');
+                        option.value = name;
+                        option.textContent = name;
+                        return option;
+                    })
+            )
+     })
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            const input = action.parentElement.querySelector('input[type="text"]');
+            input.value = '';
+            state[action.dataset.field] = '';
+        }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import {initTable} from "./components/table.js";
 // @todo: подключение
 import {initPagination} from "./components/pagination.js"
 import {initSorting} from "./components/sorting.js"
+import {initFiltering} from "./components/filtering.js"
 
 
 // Исходные данные используемые в render()
@@ -39,6 +40,7 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
+    result = applyFiltering(result, state, action);
     result = applySorting(result, state, action);
     result = applyPagination(result, state, action);
 
@@ -69,6 +71,10 @@ const applySorting = initSorting([
     sampleTable.header.elements.sortByDate,
     sampleTable.header.elements.sortByTotal
 ]);
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers
+});
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
