b82ee79a7f52bf1149f2692564a0b63c37633fa5
vladimir-rusakov <virt.p.q@yandex.ru>
2026-02-28 18:06:13 +0300

    исправление применения поиска в render

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ async function render(action) {
     let query = {}; // копируем для последующего изменения
 
     // @todo: использование
-    // result = applySearching(result, state, action);
+    query = applySearching(query, state, action);
     query = applyFiltering(query, state, action);
     // result = applySorting(result, state, action);
     query = applyPagination(query, state, action);
