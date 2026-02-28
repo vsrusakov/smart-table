0a0da072f52773e536db4f8e8957e02a74faaeb9
vladimir-rusakov <virt.p.q@yandex.ru>
2026-02-28 18:05:37 +0300

    рефакторинг функции initSearching и удаление модуля сравнения

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,21 +1,9 @@
-import {rules, createComparison} from "../lib/compare.js";
-
-
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-    const compare = createComparison(
-        ['skipEmptyTargetValues'], 
-        [
-            rules.searchMultipleFields(
-                searchField, 
-                ['date', 'customer', 'seller'], 
-                false
-            )
-        ]
-    );
-
-    return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data.filter(row => compare(row, state));
-    }
+    return (query, state, action) => {
+        return state[searchField] // проверяем, что в поле поиска было что-то введено
+            ? Object.assign({}, query, {
+                  search: state[searchField], // устанавливаем в query параметр
+              })
+            : query; // если поле с поиском пустое, просто возвращаем query без изменений
+    };
 }
